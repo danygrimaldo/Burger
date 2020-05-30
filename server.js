af3ee025d2c08e1db56f361eddb4bde8648acc2f
Your Name <danygrimaldo@gmail.com>
2020-05-30 16:26:00 -0400

    HW

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const exphbs = require("express-handlebars");
 
 // Set app to express
 const app = express();
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+app.use(routes);
 
 // Establish PORT
 var PORT = process.env.PORT || 8080;
